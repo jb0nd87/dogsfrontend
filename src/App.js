54730d3f8b72e4938d7faec720089304317aba14
jb0nd87 <68964150+jb0nd87@users.noreply.github.com>
2020-10-20 09:58:08 -0700

    changed host url

--- src/App.js
+++ src/App.js
@@ -1,35 +1,119 @@
-import React from "react";
-import "./App.css";
-import { Route, Link, Switch } from "react-router-dom";
-import Display from "./Display";
-import Form from "./Form";
+import React from 'react';
+import './App.css';
+import { Route, Link, Switch } from 'react-router-dom';
+import Display from './Display';
+import Form from './Form';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>DOG LISTING SITE</h1>
-      <hr />
-      <main>
-        <Switch>
-          <Route exact path="/" render={(rp) => <Display {...rp} />} />
-          <Route
-            exact
-            path="/create"
-            render={(rp) => (
-              <Form {...rp} label="create" dog={{}} handleSubmit={() => {}} />
-            )}
-          />
-          <Route
-            exact
-            path="/edit"
-            render={(rp) => (
-              <Form {...rp} label="update" dog={{}} handleSubmit={() => {}} />
-            )}
-          />
-        </Switch>
-      </main>
-    </div>
-  );
+	//URL variable
+	const url = 'https://dogsbackendjgb.herokuapp.com';
+	//state to hold dogs
+	const [dogs, setDogs] = React.useState([]);
+	//empty dog for form
+	const emptyDog = {
+		name: '',
+		age: 0,
+		img: '',
+	};
+
+	//selected dog for user to select a dog
+	const [selectedDog, setSelectedDog] = React.useState(emptyDog);
+
+	//function to fetch all dogs to load
+	const getDogs = () => {
+		fetch(url + '/dog/')
+			.then((response) => response.json())
+			.then((data) => {
+				setDogs(data);
+			});
+	};
+
+	//get dogs on page reload
+	React.useEffect(() => getDogs(), []);
+
+	//handleCreate function for creating dogs
+	const handleCreate = (newDog) => {
+		fetch(url + '/dog/', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newDog),
+		}).then((response) => getDogs());
+	};
+
+	//handleUpdate function for updating dogs
+	const handleUpdate = (dog) => {
+		fetch(url + '/dog/' + dog._id, {
+			method: 'put',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(dog),
+		}).then((response) => getDogs());
+	};
+
+	//selectDog which selects a dog
+	const selectDog = (dog) => {
+		setSelectedDog(dog);
+	};
+
+	//delete a dog function
+	const deleteDog = (dog) => {
+		fetch(url + '/dog/' + dog._id, {
+			method: 'delete',
+		}).then((response) => getDogs());
+	};
+
+	return (
+		<div className='App'>
+			<h1>DOG LISTING SITE</h1>
+			<hr />
+			<Link to='/create'>
+				<button>Add Dog</button>
+			</Link>
+			<main>
+				<Switch>
+					<Route
+						exact
+						path='/'
+						render={(rp) => (
+							<Display
+								{...rp}
+								dogs={dogs}
+								selectDog={selectDog}
+								deleteDog={deleteDog}
+							/>
+						)}
+					/>
+					<Route
+						exact
+						path='/create'
+						render={(rp) => (
+							<Form
+								{...rp}
+								label='create'
+								dog={emptyDog}
+								handleSubmit={handleCreate}
+							/>
+						)}
+					/>
+					<Route
+						exact
+						path='/edit'
+						render={(rp) => (
+							<Form
+								{...rp}
+								label='update'
+								dog={selectedDog}
+								handleSubmit={handleUpdate}
+							/>
+						)}
+					/>
+				</Switch>
+			</main>
+		</div>
+	);
 }
 
 export default App;
